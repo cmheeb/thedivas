a30e8efa35da48ce62cdb45ed93fda19dab85bf1
parkertackley <parkertackley@gmail.com>
2024-03-08 21:12:57 -0500

    feat: running static html page
    fix: host static css + js"

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 'use strict';
 
 const express = require('express');
+const app = express();
+const path = require('path');
+
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello shitty poo poo fart!\n')
-});
+
+app.use(express.static(path.join(__dirname, 'public')))
+// app.get('/', (req, res) => {
+//     res.send('Hello shitty poo poo fart!\n')
+// });
 
 app.listen(PORT, HOST);
 console.log(`istening on port ${PORT}`);
