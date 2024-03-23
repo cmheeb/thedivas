b935d56792e58dffb5d5444c4cc6bb12f0981d74
parkertackley <parkertackley@gmail.com>
2024-03-22 23:04:08 -0400

    feat: add register button and submit as json
    fix: hash, and store into mongodb

---
+++ public/functions.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('reg-form');
+form.addEventListener('submit', regUser);
+
+async function regUser(event) {
+    event.preventDefault();
+    const username = document.getElementById('reg-username').value;
+    const password = document.getElementById('reg-password').value;
+
+    const result = await fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    }).then((res) => res.json())
+
+    console.log(result)
+};
--- server.js
+++ server.js
@@ -2,35 +2,32 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// display favicon
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+mongoose.connect('mongodb://localhost:27017/user-creds')
 
-// nosniff header
-app.use((req, res, next) => {
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico'))); // favicon
+app.use((req, res, next) => {   // nosniff header
     res.setHeader('X-Content-Type-Options', 'nosniff');
     next();
 });
-
+app.use(bodyParser.json())  // d
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-    res.render('register')
-});
-
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     console.log(req.body)
     res.send('registered')
 });
 
-app.use((req, res) => {
-    res.status(404);
-    res.send('<h1>Error 404: Not Found</h1>');
-});
+// app.use((req, res) => {
+//     res.status(404);
+//     res.send('<h1>Error 404: Not Found</h1>');
+// });
 
 app.listen(PORT, HOST);
 console.log(`istening on port ${PORT}`);
