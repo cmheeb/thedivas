4db58701d848af7dd4d3a72ffdba60bda50ff906
parkertackley <parkertackley@gmail.com>
2024-03-22 21:49:25 -0400

    add nosniff header and register form
    register form doesnt add to any database yet

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
-'use strict';
-
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const favicon = require('serve-favicon');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+// display favicon
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 
-app.use(express.static(path.join(__dirname, 'public')))
-// app.get('/', (req, res) => {
-//     res.send('Hello shitty poo poo fart!\n')
-// });
+// nosniff header
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    next();
+});
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.render('register')
+});
+
+app.post("/register", (req, res) => {
+    console.log(req.body)
+    res.send('registered')
+});
+
+app.use((req, res) => {
+    res.status(404);
+    res.send('<h1>Error 404: Not Found</h1>');
+});
 
 app.listen(PORT, HOST);
 console.log(`istening on port ${PORT}`);
