e2799f62df0d99332ee3d2335a7a482dc870935d
parkertackley <parkertackley@gmail.com>
2024-03-25 03:06:28 -0400

    add post schema

--- model/user.js
+++ model/user.js
@@ -8,5 +8,13 @@ const userSchema = new mongoose.Schema({
     { collection: 'users'}
 );
 
+const postSchema = new mongoose.Schema({
+    username: { type: String, required: true}, 
+    post: { type: String, required: true},
+    },
+    { collection: 'chat'}
+);
+
 const User = mongoose.model('userSchema', userSchema);
-module.exports = User;
+const Post = mongoose.model('postSchema', postSchema);
+module.exports = {User, Post};
